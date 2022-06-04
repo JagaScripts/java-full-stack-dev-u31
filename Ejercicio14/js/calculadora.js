adfb7a17e37bf06c9da1b0bb0d8b09f16bc8708b
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-06-05 00:17:40 +0200

    Calculadora acabada

--- Ejercicio14/js/calculadora.js
+++ Ejercicio14/js/calculadora.js
@@ -1,75 +1,88 @@
 var botones = document.getElementsByTagName("button");
 var input = document.getElementById("resultado");
-var p = document.getElementsByTagName("p");
+var p = document.getElementsByTagName("p")[0];
 input.disabled = true;
 var masMenos = true;
 var inverso = false;
 
-for (const boton of botones) {
-    boton.addEventListener("click", function operacion(params) {
-        
-        if(boton.innerHTML === "="){
+for (const params of botones) {
+    params.addEventListener("click", function(){operacion(params)});
+};
 
-            try {
+function operacion(params) {
 
-                input.value = eval(input.value);
+    if(params.innerHTML === "=" || params.code === "NumpadEnter" || params.code === "Enter"){
 
-            } catch (error) {
-                
-                p.innerHTML = error;
+        try {
 
-            }
+            input.value = eval(input.value);
 
-        } else if(boton.id === "Raiz"){
-
-            input.value = Math.sqrt(input.value);
-
-        } else if(boton.innerHTML === "%"){
-
-            input.value = input.value/100;
-
-        } else if(boton.innerHTML === "+/-"){
-
-            if (masMenos == true) {
-                
-                masMenos = false;
-                input.value = `-${input.value}`;
-
-            } else {
-                
-                masMenos = true;
-                input.value = input.value.replace(/^-/, '');
-
-            }
-
-        } else if(boton.innerHTML === "C"){
+        } catch (error) {
             
-            input.value = "";
-            p.innerHTML = "";
-
-        } else if(boton.innerHTML === "1/x"){
-            
-            if (inverso == false) {
-                
-                inverso = true;
-                input.value = `1/${input.value}`;
-
-            } else {
-                
-                inverso = false;
-                input.value = input.value.replace(/^1\//, '');
-
-            }
-        
-        }else if(boton.innerHTML === "."){
-
-            input.value += `${input.value}.`;
-
-        } else {
-
-            input.value += boton.innerHTML;
+            p.innerHTML = `La expresión introducida no se puede evaluar, intentalo de nuevo pulsa el boton C`;
 
         }
+
+    } else if(params.id === "Raiz"){
+
+        input.value = Math.sqrt(input.value);
+
+    } else if(params.innerHTML === "%"){
+
+        input.value = input.value/100;
+
+    } else if(params.innerHTML === "+/-"){
+
+       
+        if (masMenos == true) {
+            
+            masMenos = false;
+            input.value = `-${input.value}`;
+
+        } else {
+            
+            masMenos = true;
+            input.value = input.value.replace(/^-/, '');
+
+        }
+
+    } else if(params.innerHTML === "C" || params.code === "Delete"){
         
-    });
-};
+        input.value = "";
+        p.innerHTML = "";
+
+    } else if(params.innerHTML === "1/x"){
+        
+        if (inverso == false) {
+            
+            inverso = true;
+            input.value = `1/${input.value}`;
+
+        } else {
+            
+            inverso = false;
+            input.value = input.value.replace(/^1\//, '');
+
+        }
+    
+    }else if(params.innerHTML === "."){
+
+        input.value += `${input.value}.`;
+
+    } else {
+
+       
+        if (params.innerHTML != undefined) {
+
+            input.value += params.innerHTML;
+            
+        } else if (params.code.substring(0, 4) === "Digi" || params.code.substring(0, 6) === "Numpad") {
+
+            input.value += params.key;
+
+        } 
+
+    }
+    
+}
+document.addEventListener('keydown',(event) => {operacion(event)},false);
